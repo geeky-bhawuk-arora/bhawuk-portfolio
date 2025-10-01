a6f3603f74c27c6de31ea3d9cfcacd06d3774019
Bhawuk Arora <bhawukarora042@gmail.com>
2025-10-01 16:13:54 +0530

    edited content in  hero !!

--- src/data/personalInfo.js
+++ src/data/personalInfo.js
@@ -2,10 +2,10 @@ export const personalInfo = {
   name: "Bhawuk Arora",
   title: "Aspiring Big Data Engineer",
   location: "India",
-  bio: "",
+  bio: "On paper, I'm a student of AI & Data Science. In reality, I'm a professional bug creator and a full-time enthusiast of things that fly (and occasionally crash).",
   email: "bhawukarora042@gmail.ocn",
-  phone: "+91 XXXXX XXXXX",
+  phone: "+91 97821760005",
   linkedin: "https://linkedin.com/in/bhawuk-arora",
-  github: "https://github.com/bhawuk-arora",
-  resumeUrl: "/resume.pdf"
+  github: "https://github.com/geeky-bhawuk-arora",
+  resumeUrl: "/https://drive.google.com/drive/folders/1_CUKnOHzB8MlVrgisnUR22m2OXwQDl4o?usp=drive_link"
 };
